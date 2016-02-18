0e5a481136c7456fde14c7d794432848048b61e0
Viriatus <daniel.astakhov@gmail.com>
2016-02-18 02:31:03 -0500

    Committed changes

--- filters/test.js
+++ filters/test.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
+var testString = "ass";
 
 var jsonContent;
-module.exports = function (Messages){
+module.exports = function (testString){
 	if(!jsonContent) {jsonContent = JSON.parse(fs.readFileSync("data/profanities.json"))};
 
 	return (jsonContent.indexOf(testString)>-1);
