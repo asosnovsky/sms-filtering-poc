faaefe91062eb19e7a400f77393c5171fe91e2a9
Ariel Sosnovsky <Ariel Sosnovsky>
2016-02-18 03:40:35 -0500

    filter-list endpoint

--- server.js
+++ server.js
@@ -60,13 +60,17 @@ var classData = {
 app.get('/text', function(req, res) {
 	getMsg(function(err,data){
 		var query = url.parse(req.url, true).query;
-		sendMsg(query.phone,query.msg,function(err){
-			if(!err) {
-				res.send("success");
-			}	else	{
-				res.send(err);
-			}
-		});
+		if(query.phone && query.msg) 	{
+			sendMsg(query.phone,query.msg,function(err){
+				if(!err) {
+					res.send("success");
+				}	else	{
+					res.send(err);
+				}
+			});
+		}	else	{
+			res.send("Failed, phone and msg must be defined.");
+		}
 	});
 });
 
@@ -74,6 +78,10 @@ app.get('/get', function(req, res) {
 	getMsg(function(err,data){
 		res.json(contactsData.make(data.usr));
 	});
+});
+
+app.get('/filter_list',function(req,res){
+	res.json(Object.keys(filters));
 });
 
 app.listen(port);
