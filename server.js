09bcd8ef700f1c8f5dda3e03e329202f76f5756d
Ariel Sosnovsky <Ariel Sosnovsky>
2016-02-18 11:42:01 -0500

    added tester

--- server.js
+++ server.js
@@ -81,16 +81,27 @@ app.get('/text', function(req, res) {
 			if(!bad) {
 				sendMsg(query.phone,query.msg,function(err,data){
 					if(!err) {
-						res.send("success");
+						res.json({status:"success",contact:contact});
 					}	else	{
-						res.send(err);
+						res.send({status:err,contact:contact});
 					}
 				});
 			}	else 	{
-				res.send("bad message");
+				res.send({status:"fail",contact:contact});
 			}
 		}	else	{
 			res.send("Failed, phone and msg must be defined.");
+		}
+	});
+});
+
+app.get('/forcetext', function(req,res){
+	var query = url.parse(req.url, true).query;
+	sendMsg(query.phone,query.msg,function(err,data){
+		if(!err) {
+			res.send("success");
+		}	else	{
+			res.send(err);
 		}
 	});
 });
@@ -104,6 +115,19 @@ app.get('/get', function(req, res) {
 app.get('/filter_list',function(req,res){
 	res.json(Object.keys(filters));
 });
+
+app.get('/test',function(req,res){
+	res.sendfile('index.html');
+})
+/*HTTP*/
+// var http = require("http");
+// http.createServer(function (request, response) {
+//       response.writeHead(200, {
+//          'Content-Type': 'text/plain'
+//       });
+//       response.write('Simple Simple Fun')
+//       response.end();
+// }).listen(5002);
 
 app.listen(port);
 console.log('Interceptor Server is on ' + port);
