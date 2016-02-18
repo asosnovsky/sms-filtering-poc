23fd2727eb04a5cc8ab2b2d1d84f317283ead034
Viriatus <daniel.astakhov@gmail.com>
2016-02-18 03:30:10 -0500

    Very updated profanities

--- filters/absfilters.js
+++ filters/absfilters.js
@@ -22,7 +22,7 @@ module.exports.alchohol = function (testString){
 }
 
 module.exports.drugs = function (testString){
-	return  filterMethod(testString,["cocaine","heroin","meth","molly","MDMA","ecstasy" 
+	return  filterMethod(testString,["cocaine","heroin","meth","molly","MDMA","ecstasy", 
 										"tripping balls","tripping","shrooms","magic mushrooms","dope"]);
 }
 
@@ -31,22 +31,27 @@ module.exports.marijuana = function (testString){
 }
 
 module.exports.criminalActivity = function (testString){
-	return  filterMethod(testString,["steal","stole","rape","crime","carjack","incarcerated","jailed","grand theft auto","murder"
-		                            	"restraining order"]);
+	return  filterMethod(testString,["steal","stole","rape","crime","carjack","incarcerated","jailed","grand theft auto","murder",
+		                            	"restraining order","stalk"]);
 }
 
 module.exports.sexism = function (testString){
-	return  filterMethod(testString,["bitch","whore","slut","toots","tootsie","chick","prostitute"]);
+	return  filterMethod(testString,["bitch","whore","slut","toots","tootsie","chick","prostitute","babe","hotstuff","boytoy"]);
 }
 
 module.exports.homophobic = function (testString){
-	return  filterMethod(testString,["fag","faggot","homo","dyke","butch","prison bitch"]);
+	return  filterMethod(testString,["fag","faggot","homo","dyke","butch","prison bitch","tranny"]);
 }
 module.exports.miscInsults = function (testString){
 	return filterMethod(testString,["retard","bastard","sped","idiot","moron","dumb"]);
 }
 
 module.exports.politicalReferences = function (testString){
-	return filterMethod(testString,["communist","capitalist","fascist","trump","sanders"]);
+	return filterMethod(testString,["communist","capitalist","fascist","trump","sanders","feminism","feminist","holocaust denial"]);
 }
 
+module.exports.sexualReferences = function (testString){
+	return filterMethod(testString,["dildo","anal","pussy","dick","cunnilingus","BBW","tits","blowjob","bondage",
+		                       "bukkake","creampie","cumshots","dp","double penetraion","backroom casting couch",
+		                        "fetish","ebony","fisting","gangbang","handjob","hentai","interracial","masturbation","MILF","orgy"]);
+}
